79a2f5663ed68b4a77e4b3e2fe900c0046604641
Herlander Silva <130112009+herlandersilva@users.noreply.github.com>
2023-04-20 10:46:57 +0100

    Challenge of the day: Handle arithmetic exception ( https://hyperskill.org/learn/daily/7605 )
    Signed-off-by: Herlander Silva <130112009+herlandersilva@users.noreply.github.com>

--- Topics/Taming exceptions/Handle arithmetic exception/src/Main.kt
+++ Topics/Taming exceptions/Handle arithmetic exception/src/Main.kt
@@ -1,3 +1,5 @@
-fun main() {
-    // put your code here
+fun main() = try {
+    (readln().toInt() / readln().toInt()).let(::println)
+} catch (_: ArithmeticException) {
+    "Division by zero, please fix the second argument!".let(::println)
 }
